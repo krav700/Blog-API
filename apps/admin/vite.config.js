27ed9e14058db981e81028faf709daa93d2cc0f0
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2026-09-22 17:51:20 +0300

    fix: swap to new proxy url

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
         proxy: {
-            "/api": "http://localhost:3000",
+            "/api": "https://blog-api-sxu4.onrender.com",
         },
     },
 });
